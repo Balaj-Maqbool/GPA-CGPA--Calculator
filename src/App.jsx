9bae28d16f7964fc3782b374a9ce5ec62dab24c9
Balaj Maqbool <balajmaqbool54@gmail.com>
2025-02-07 21:14:08 +0500

    adding lowernavbar

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
           element={
             <>
               <Navbar />
-              {/* <LowerNavbar/> */}
+              <LowerNavbar/>
               <GPA_calculator />
             </>
           }
